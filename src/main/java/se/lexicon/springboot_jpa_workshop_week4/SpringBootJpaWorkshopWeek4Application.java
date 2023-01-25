09017bf08aaf0a917a9a80d1074cfb8882b42966
MariusAdrianStoica <stoichitza@yahoo.com>
2023-01-25 10:31:26 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/springboot_jpa_workshop_week4/SpringBootJpaWorkshopWeek4Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.springboot_jpa_workshop_week4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaWorkshopWeek4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaWorkshopWeek4Application.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/springboot_jpa_workshop_week4/SpringBootJpaWorkshopWeek4ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.springboot_jpa_workshop_week4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaWorkshopWeek4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
